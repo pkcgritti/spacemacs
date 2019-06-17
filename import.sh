b8c5d1579d2debeb0fe621ae22746b8893cd399d
Marcos Cesar Gritti <cesargritti@gmail.com>
2019-06-17 00:56:49 -0300

    Spacemacs config

---
+++ import.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Copying .spacemacs file"
+mkdir -p src/
+cp ~/.spacemacs src/spacemacs
+
+echo "Done"
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying .spacemacs file"
+cp src/spacemacs ~/.spacemacs
+
+echo "Done"
